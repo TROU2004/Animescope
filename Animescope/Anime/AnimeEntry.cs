f04e8f7e111b282d6a93b1eddf37cc4955795ec9
TurOU <chenbohan911@163.com>
2021-11-28 21:36:19 +0800

    添加项目文件。

---
+++ Animescope/Anime/AnimeEntry.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animescope.Datacollect
+{
+    internal class AnimeEntry
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CreateMethods CreateMethod { get; private set; }
+        public string Labels { get; set; }
+        public string State { get; set; }
+        public string PicURL { get; set; }
+
+        private AnimeEntry() { }
+        public static AnimeEntry FromTopHtml(HtmlNode node) {
+            return new AnimeEntry()
+            {
+                CreateMethod = CreateMethods.TOP,
+                Title = node.SelectSingleNode("./h2").InnerText,
+                Description = node.SelectSingleNode("./p").InnerText,
+                State = node.SelectSingleNode("./span[1]").InnerText,
+                Labels = node.SelectSingleNode("./span[2]").InnerText,
+                PicURL = node.SelectSingleNode("./a/img").Attributes["src"].Value,
+            };
+            
+        }
+
+        public enum CreateMethods { TOP }
+    }
+}
---
+++ Animescope/Anime/DataSourceYHDM.cs
@@ -0,0 +1,20 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animescope.Datacollect
+{
+    internal class DataSourceYHDM
+    {
+        static readonly string URL = "http://www.yinghuacd.com/";
+        public IEnumerable<AnimeEntry> GetTopData()
+        {
+            var doc = new HtmlWeb().LoadFromWebAsync(URL, Encoding.UTF8).Result;
+            var list = doc.DocumentNode.SelectNodes("/html/body/div[9]/div[2]/div[3]/div[2]/ul");
+            return list.Select(node => AnimeEntry.FromTopHtml(node));
+        }
+    }
+}
